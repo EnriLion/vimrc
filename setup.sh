843cd977b0f8d13974620300ba7649440c7611e1
EnriqueLion <enriqueleon0201leon@gmail.com>
2022-12-27 20:26:57 -0600

    Edit setup.sh

--- setup.sh
+++ setup.sh
@@ -6,12 +6,13 @@ if [[ "$system" == *"microsoft"* ]]; then
     echo "Operating System = WSL"
  
     #Create the file and settings
-    
+ touch ~/.vimrc
 
 elif [[ "$system" == *"linux"* ]]; then
     echo "Operating System = Linux"
 
     #Create the file and settings
+ touch ~/.vimrc
 
 else
     echo "Operating System = Unknown"

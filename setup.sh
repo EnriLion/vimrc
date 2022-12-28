2770df0d35a24e073cd8e1c445965e1e6aa8ffeb
EnriqueLion <enriqueleon0201leon@gmail.com>
2022-12-27 20:12:24 -0600

    Updating setup.sh

--- setup.sh
+++ setup.sh
@@ -3,9 +3,16 @@ system=$(uname -r)
 
 # Check the operating system
 if [[ "$system" == *"microsoft"* ]]; then
-    echo "Operating System = WSL" 
+    echo "Operating System = WSL"
+ 
+    #Create the file and settings
+    
+
 elif [[ "$system" == *"linux"* ]]; then
     echo "Operating System = Linux"
+
+    #Create the file and settings
+
 else
     echo "Operating System = Unknown"
 fi

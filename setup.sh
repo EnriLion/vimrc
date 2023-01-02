b44d09f805a6d507e01202ee73a7d208cdda22e0
EnriqueLion <enriqueleon0201leon@gmail.com>
2023-01-01 22:17:44 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+
 system=$(uname -r)
+#function to install vim plug
+function plug {
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+}
 
 # Check the operating system
 if [[ "$system" == *"microsoft"* ]]; then
     echo "Operating System = WSL" 
+    plug
 elif [[ "$system" == *"linux"* ]]; then
     echo "Operating System = Linux"
 else

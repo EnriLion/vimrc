e5ef811a6a2532d0710511a953ceceaaec2206e4
EnriqueLion <enriqueleon0201leon@gmail.com>
2023-01-01 22:55:06 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,14 +10,45 @@ function plug {
 
 }
 
+#function overwritevim
+
+function overwritevim {
+	cat > .vimrc
+	"Vim configuration
+	set number
+	"Vim-Plug
+	call plug#begin('~/.vim/plugged')
+
+	Plug 'wakatime/vim-wakatime'
+
+	Plug 'mattn/emmet-vim'
+
+	Plug 'joshdick/onedark.vim'
+
+	Plug'itchyny/lightline.vim'
+
+	Plug '907th/vim-auto-save'
+
+	#Plug 'preservim/nerdtree'
+
+        #Plug 'ryanoasis/vim-devicons' 
+
+	call plug#end()
+
+}
+
+
+
 #function to install vim plug
 
 function existvimrc {
 
 if test ! -f /home/$USER ; then
-	
+      overwritevim
 else
-	touch /home/$USER/.vimrc
+    touch /home/$USER/.vimrc
+    overwritevim
+
 
 
 fi
@@ -26,10 +57,12 @@ fi
 
 
 
+
 # Check the operating system
 if [[ "$system" == *"microsoft"* ]]; then
     echo "Operating System = WSL" 
     existvimrc 
+    overwritevim
  
     #Create the file and settings
  touch ~/.vimrc
